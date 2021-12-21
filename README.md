1f95a51cc3ce1274281467907c2a530b633a1211
yoonadev <yoonadev@gmail.com>
2021-12-21 11:43:43 +0900

    Take2andAdd

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AlgorithmPractice
 ## BOJ
-- 셀프넘버 : [Q4673](src/backjoon/Q4673.java)
-##Programmers
-- [예산](https://programmers.co.kr/learn/courses/30/lessons/12982) : [Budget](src/programmers/Budget.java)
+- 셀프넘버 : [Q4673.java](src/backjoon/Q4673.java)
+## Programmers
+- [예산](https://programmers.co.kr/learn/courses/30/lessons/12982) : [Budget.java](src/programmers/Budget.java)
+- [두 개 뽑아서 더하기](https://programmers.co.kr/learn/courses/30/lessons/68644) : [Take2andAdd.java](src/programmers/Take2andAdd.java) (SetToArray)
---
+++ src/programmers/Take2andAdd.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Take2andAdd {
+	public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < numbers.length; i++) {
+        	for(int j = i + 1; j < numbers.length; j++) {
+        		set.add(numbers[i] + numbers[j]);
+        	}
+        }
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for(Integer s : set) {
+        	answer[idx] = s;
+        	idx++;
+        }
+        Arrays.sort(answer);
+        return answer;
+//        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
