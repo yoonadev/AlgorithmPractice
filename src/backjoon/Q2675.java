9855f637b28c6dc53aaa2f161b5425502c4a71d6
yoonadev <yoonadev@gmail.com>
2022-01-05 15:13:10 +0900

    Q2675

---
+++ src/backjoon/Q2675.java
@@ -0,0 +1,44 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제
+문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+
+QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다. 
+
+출력
+각 테스트 케이스에 대해 P를 출력한다.
+ */
+public class Q2675 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine()); //테스트케이스 수
+		
+		for(int i = 0; i < t; i++) {
+			String a = br.readLine();
+			st = new StringTokenizer(a);
+			
+			int r = Integer.parseInt(st.nextToken()); //반복 횟수
+			String s = st.nextToken(); //대상 문자열
+			
+			//s의 각 문자 r번 반복 출력
+			for(int j = 0; j < s.length(); j++) {
+				for(int k = 0; k < r; k++) {
+					System.out.print(s.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+}
